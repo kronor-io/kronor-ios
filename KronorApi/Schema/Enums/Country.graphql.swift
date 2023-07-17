aeaa31db36f61d552ad3c71a182ec0dc95eec608
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2023-07-17 16:59:44 +0200

    include missing file

---
+++ KronorApi/Schema/Enums/Country.graphql.swift
@@ -0,0 +1,40 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+import ApolloAPI
+
+public extension KronorApi {
+  /// Supported countries
+  enum Country: String, EnumType {
+    case at = "AT"
+    case be = "BE"
+    case ch = "CH"
+    case cy = "CY"
+    case cz = "CZ"
+    case de = "DE"
+    case dk = "DK"
+    case ee = "EE"
+    case es = "ES"
+    case fi = "FI"
+    case fr = "FR"
+    case gr = "GR"
+    case hr = "HR"
+    case hu = "HU"
+    case ie = "IE"
+    case `is` = "IS"
+    case it = "IT"
+    case lt = "LT"
+    case lu = "LU"
+    case lv = "LV"
+    case mt = "MT"
+    case nl = "NL"
+    case no = "NO"
+    case pl = "PL"
+    case pt = "PT"
+    case ro = "RO"
+    case se = "SE"
+    case si = "SI"
+    case sk = "SK"
+  }
+
+}
