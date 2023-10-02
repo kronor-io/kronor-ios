3c955a6ca8474c2fddad581498fdc8165fb8547f
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2023-10-02 11:37:30 +0200

    fix formatting

--- KronorComponents/Common/Preview.swift
+++ KronorComponents/Common/Preview.swift
@@ -35,7 +35,7 @@ enum Preview {
             networking: networking,
             paymentMethod: paymentMethod,
             returnURL: returnURL,
-            onPaymentFailure: {reason in },
+            onPaymentFailure: { reason in },
             onPaymentSuccess: { paymentId in }
         )
     }
@@ -57,7 +57,7 @@ enum Preview {
             stateMachine: machine,
             networking: networking,
             returnURL: returnURL,
-            onPaymentFailure: {reason in },
+            onPaymentFailure: { reason in },
             onPaymentSuccess: { paymentId in }
         )
     }
